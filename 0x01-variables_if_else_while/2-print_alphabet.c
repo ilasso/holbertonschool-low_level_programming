9998ced5e842a1cdb2fad8c7d219d75fb207f3cf
ilasso <1204@holbertonschool.com>
2019-09-19 20:09:52 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 97;
+
+for (n = 97; n < 123; n++)
+{
+	putchar(n);
+}
+	putchar(10);
+
+
+
+	return (0);
+}

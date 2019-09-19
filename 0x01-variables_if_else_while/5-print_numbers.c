a4fa65ef3bc98a6719380f8880c750f9a674d014
ilasso <1204@holbertonschool.com>
2019-09-19 20:38:30 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 97;
+
+for (n = 48; n < 58; n++)
+{
+	putchar(n);
+}
+	putchar(10);
+
+
+
+	return (0);
+}

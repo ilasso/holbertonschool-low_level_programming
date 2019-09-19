59a01782d0358e4339f17206b4e8c7474b862406
ilasso <1204@holbertonschool.com>
2019-09-19 21:17:31 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 97;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+
+	putchar(10);
+
+
+
+	return (0);
+}

eaccfa7cf68ec64a91b5c67a382576d1dfc1862e
ilasso <1204@holbertonschool.com>
2019-09-19 21:59:09 +0000

    10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -9,6 +8,7 @@
  */
 int main(void)
 {
+
 int n = 97;
 int m = 97;
 
@@ -36,7 +36,6 @@ int m = 97;
 
 
 	}
-
 
 	putchar(10);
 

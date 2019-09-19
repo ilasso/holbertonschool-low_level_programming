1c76315d2c6e95119780b1f601554de360438a7f
ilasso <1204@holbertonschool.com>
2019-09-19 20:33:35 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 97;
+
+for (n = 97; n < 123; n++)
+{
+	if (n != 101)
+	{
+		if (n != 113)
+		{
+			putchar(n);
+		}
+	}
+}
+	putchar(10);
+
+
+
+	return (0);
+}

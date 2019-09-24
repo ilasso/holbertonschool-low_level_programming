34cdad4a51e26003e76fcbf21604c475fee5fc12
ilasso <1204@holbertonschool.com>
2019-09-24 19:01:55 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add-  return abs value to a number
+ * @: argument input to evaluate
+ * @: argument input to evaluate
+ * Return: last digit
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b)
+{
+
+	return (a + b);
+
+}

1280b4cbe0641181ec76c85086b12f538ab42dfe
ilasso <1204@holbertonschool.com>
2019-09-24 05:10:10 +0000

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _islower - say if a character is lower or not
+ * @c: - charatecte to evaluate
+ *
+ * Return: 1 if char is lower, else return 0
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}

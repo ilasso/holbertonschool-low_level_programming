b112a6894754ac14ba5c5e9e71f2176b44dad49a
ilasso <1204@holbertonschool.com>
2019-09-24 05:47:57 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign -  print the sign of a number
+ * @n: argument input to evaluate
+ * Return: 1 if n is greater than zero, 0 if n is zero,
+ * -1 if n is less than zero
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+		return (-1);
+
+}

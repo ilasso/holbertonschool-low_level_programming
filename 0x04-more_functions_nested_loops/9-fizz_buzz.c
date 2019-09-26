bdc7b3f680c27eafca0b93c8d21abd64a9ab2106
ilasso <1204@holbertonschool.com>
2019-09-26 20:29:11 +0000

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+
+		if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+
+		if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+
+		printf("%d ", i);
+
+	}
+
+	printf("\n");
+	return (0);
+}

12f70b13928a15f38ae6f0f0393806ef06068a97
ilasso <1204@holbertonschool.com>
2019-09-27 01:46:22 +0000

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,6 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
-	int last;
 
 	if (size <= 0)
 	{
@@ -25,19 +24,19 @@ void print_triangle(int size)
 		return;
 	}
 
-	last = size;
 
-	for (i = 0; i < size ; i++)
+	for (i = 1; i <= size ; i++)
 	{
 		for (j = 0; j < size  ; j++)
 		{
-			if (last < size) {
+			if (j < (size - i))
+			{
 				_putchar (' ');
 			}
-			else {
+			else
+			{
 			_putchar ('#');
 			}
-		last -= size;
 		}
 			_putchar ('\n');
 

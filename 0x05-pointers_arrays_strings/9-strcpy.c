26b9e06bfe89a6c278d901333221691c2a1407bb
ilasso <1204@holbertonschool.com>
2019-10-02 05:47:54 +0000

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy    - function that copy strings
+ * @dest   : string dest
+ * @src   : string origin
+ * Return:  void
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+
+
+	}
+
+	return (dest);
+
+
+}

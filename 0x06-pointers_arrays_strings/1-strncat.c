7a5f8d779e57a83245973867617a0c38a45e3b37
ilasso <1204@holbertonschool.com>
2019-10-03 17:49:47 +0000

    0-1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 
 /**
- * _strcat    - function that concatenate src to dest
+ * _strncat    - function that concatenate n chars of src to dest
  * @dest   : string dest
  * @src   : string origin
+ * @n : quantity characters to add a dest
  * Return:   pointer to char
  * On error, -1 is returned, and errno is set appropriately.
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i = 1;
 	char *a;
 
 	a = dest; /*save inic address dest to goback */
@@ -17,20 +18,22 @@ char *_strcat(char *dest, char *src)
 
 	while (*dest != '\0')
 	{
-		i++; /* obtain null character */
 		dest++; /*go to last position of array */
 
 		if (*dest == '\0') /* evaluate final firts string to add another*/
 		{
-			while (*src != '\0')
+			while (*src != '\0' && i <= n) /* only add n characteres src to dest*/
 			{
 				*dest = *src;	/*reclace \0 whit firts character other string */
 				dest++;
 				src++;
+				i++;
 			}
 		}
 
 	}
+
+	*dest = '\0'; /*put last character to "new" dest */
 
 	dest = a; /* goback to the original position */
 

722772af34d487db711c3b56b8ce2fb15ea73c67
ilasso <1204@holbertonschool.com>
2019-10-04 02:30:20 +0000

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <strings.h>
+
+/**
+ * reverse_array  - function that concatenate n chars of src to dest
+ * @a : array integers
+ * @n    : number of elements of the array
+ * Return:  integer
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void reverse_array(int *a, int n)
+{
+	int sw = 0;
+	int i = 0;
+
+	while (i <= n)
+	{
+		sw = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = sw;
+		i++;
+		n--;
+	}
+
+}

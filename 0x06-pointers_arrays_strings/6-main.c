14721d74fe8fe831fcafbfd5c4c29effc3f5250a
ilasso <1204@holbertonschool.com>
2019-10-05 19:02:21 +0000

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    /*char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";*/
+    /*char s[] = "hello world! expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";*/
+    /*char s[] = "HELLO WORLD! expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";*/
+/*    char s[] = "-- expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";*/
+    char s[] = "";
+    char *p;
+
+    p = cap_string(s);
+    printf("%s", p);
+    printf("%s", s);
+    return (0);
+}

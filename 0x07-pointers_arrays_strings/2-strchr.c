9ae128553d285783db3cb7cadd2e477e8f3d206f
ilasso <1204@holbertonschool.com>
2019-10-07 18:10:43 +0000

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <strings.h>
+
+/**
+ * _strchr  - function that locates a character in a string.
+ * @s :   input string
+ * @c :  character to find firts occurrence
+ * Return:  pointer a char
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+
+	}
+return (NULL);
+}

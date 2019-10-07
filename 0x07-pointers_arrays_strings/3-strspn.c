b4e266c8bb6861c2b6d4dcec8cfffe3e6218d417
ilasso <1204@holbertonschool.com>
2019-10-07 20:04:54 +0000

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <strings.h>
+
+/**
+ * _strspn  - function that gets the length of a prefix substring.
+ * @s :   input string
+ * @accept :  character to find firts occurrence
+ * Return:  Returns the number of bytes in the initial segment of s which
+ *		consist only of bytes from accept
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	int counter = 0;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				counter++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+	}
+
+return (counter);
+}

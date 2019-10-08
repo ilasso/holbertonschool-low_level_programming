ac7d0d6e09d3ae6ae7b50401d1a34fb75b5054ae
ilasso <1204@holbertonschool.com>
2019-10-08 22:34:06 +0000

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <strings.h>
+
+/**
+ * print_chessboard -  function that prints the chessboard.
+ * @a :   pointer to array
+ * Return: void
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+
+	}
+
+}

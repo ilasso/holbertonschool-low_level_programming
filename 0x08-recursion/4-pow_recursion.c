7441929c6545343b5b87c4d5ca12dff9d8cae288
ilasso <1204@holbertonschool.com>
2019-10-09 19:57:39 +0000

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,12 +13,9 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
-	{
-		return (1);
-	}
+	int i;
 
-	if (y == 1)
+	if (y == 0)
 	{
 		return (1);
 	}
@@ -27,5 +24,6 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	return (x * _pow_recursion(x, y - 1));
+	i = y - 1;
+	return (x * _pow_recursion(x, i));
 }

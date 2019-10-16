476827aa9445719012e05e60f59304efd4df06b8
ilasso <1204@holbertonschool.com>
2019-10-16 03:07:58 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,51 +9,39 @@
  * Return:   pointer to char. string cat
  * On error, -1 is returned, and errno is set appropriately.
  */
-/*char *_strncat(char *dest, char *src, int n)*/
 char *str_concat(char *s1, char *s2)
 {
 	int i = 0;
 	int j = 0;
 	char *a;
-	int len1;
-	int len2;
-	int sizes1; 
-	int sizes2; 
+	int len1 = 0;
+	int len2 = 0;
 
-
-	/* len s1 */
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 	;
-
-	/* len s2 */
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 	;
 
-	sizes1 = sizeof(char) * (len1 + 1);
-	sizes2 = sizeof(char) * (len2 + 1);
+	a = (char *)malloc(((len1 + 1) * sizeof(char)) + ((len2 + 1) * sizeof(char)));
 
-
-	a = (char *) malloc(sizes1 + sizes2);
-
-	if ( a == NULL)
+	if (a == NULL)
 	{
 		return (NULL);
 	}
-
-
-
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && !(s1 == NULL))
 	{
 		a[i] = s1[i];
 		i++;
 	}
-
-	while (s2[j] != '\0')
+	while (s2[j] != '\0' && !(s2 == NULL))
 	{
 		a[i] = s2[j];
 		j++;
 		i++;
 	}
-
 	return (a);
 }

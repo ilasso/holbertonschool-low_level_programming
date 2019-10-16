e57717bfd8ea44989a0f11ebe302b6bae6ce135e
ilasso <1204@holbertonschool.com>
2019-10-16 04:15:47 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		a[i] = (int *) malloc(height * sizeof(int));
+		a[i] = (int *) malloc(width * sizeof(int));
 		if (a[i] == NULL)
 			return (NULL);
 	}

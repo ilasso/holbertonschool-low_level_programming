36fa6bc3fcf9c79fa522d891c0af22a1322c014b
ilasso <1204@holbertonschool.com>
2019-10-21 21:55:30 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structc dog
+ * @name: Dog Name
+ * @age: Dog age
+ * @owner: Dog's Owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */

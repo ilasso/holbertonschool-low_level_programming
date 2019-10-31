8a9eaee88170c8871f73166224ea3479ff39afe1
ilasso <1204@holbertonschool.com>
2019-10-31 21:04:16 +0000

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,13 +5,14 @@
 /**
  * add_node - function that adds a new node at the beginning of a list_t list.
  * @head:  pointer to last node created, or inic list
+ * @str:  string to store to node
  * Return:  pointer to new element
  * On error, return NULL
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-
+	size_t i;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -19,9 +20,10 @@ list_t *add_node(list_t **head, const char *str)
 		printf("Error\n");
 		return (NULL);
 	}
-
+	for (i = 0; str[i] != '\0'; i++)
+	;
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = i;
 	new->next = *head;
 	*head = new;
 	return (new);

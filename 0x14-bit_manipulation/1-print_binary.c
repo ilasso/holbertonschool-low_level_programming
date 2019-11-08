e712261f4294393dc2adbced3991e2270437c9b9
ilasso <1204@holbertonschool.com>
2019-11-08 01:09:07 +0000

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,73 +2,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-listint_t *add_nodeint_end(listint_t **head, const int n);
-
 /**
  * print_binary-function that prints the binary representation of a number.
- *		 a linked listint_t list.
  * @n:  decimal to print binary notation
- * Return: len of a list
+ * Return: void
  * On error, return NULL
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int a=1;
-	unsigned long int b=0;
+	unsigned long int a = 1;
+	unsigned long int b = 0;
+	unsigned long int maindigit;
+	int count = 0;
 
-	while ( b < n)
+	if (n == 0)
+	{
+		_putchar(48);
+		return;
+	}
+	/* find binary digit most significative */
+	while (b < n)
 	{
 		b += n & a;
-		/*printf("shif 1 a=%lu res=%lu\n", a,n&a);*/
-		if(n&a)
+		a = a << 1;
+		count++;
+	}
+	maindigit = 1 << (count - 1); /* most singniticative true bit*/
+
+	/* evaluate bit to bit to print*/
+
+	while (maindigit > 0)
+	{
+
+		if (b & maindigit)
 			_putchar(49);
 		else
 			_putchar(48);
-	
-		a = a<<1;
-	}
 
-	_putchar('\n');
-}
-
-/**
- * add_nodeint_end-function that adds new node at the end of a listint_t list.
- * @head:  pointer to head of a list
- * @n:  data int of new element
- * Return: size of a list
- * On error, return NULL
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-
-	listint_t *new;
-	listint_t *temp;
-
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (NULL);
-	}
-
-	new->n = n;
-	if (*head == NULL)
-	{
-		new->next = *head;
-		*head = new;
-	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-
-		}
-			temp->next = new;
+		maindigit = maindigit >> 1;
 
 	}
 
-	return (new);
 }

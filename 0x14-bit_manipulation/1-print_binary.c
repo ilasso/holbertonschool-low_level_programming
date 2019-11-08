2d7dea9ae546f2d7ca2310c64d76572bdd0f6814
ilasso <1204@holbertonschool.com>
2019-11-08 03:03:23 +0000

    Control INT_MAX

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
 /**
  * print_binary-function that prints the binary representation of a number.
@@ -13,7 +14,7 @@ void print_binary(unsigned long int n)
 	unsigned long int a = 1;
 	unsigned long int b = 0;
 	unsigned long int maindigit;
-	int count = 0;
+	unsigned long int count = 0;
 
 	if (n == 0)
 	{
@@ -27,14 +28,18 @@ void print_binary(unsigned long int n)
 		a = a << 1;
 		count++;
 	}
+
 	maindigit = 1 << (count - 1); /* most singniticative true bit*/
+
+	if (n > INT_MAX)
+		maindigit = n;
 
 	/* evaluate bit to bit to print*/
 
 	while (maindigit > 0)
 	{
 
-		if (b & maindigit)
+		if (n & maindigit)
 			_putchar(49);
 		else
 			_putchar(48);

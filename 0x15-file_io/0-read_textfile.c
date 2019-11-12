e7a128412e28442d3c63aeb046e8239802a64ddd
ilasso <1204@holbertonschool.com>
2019-11-12 23:02:20 +0000

    0-read_textfile.c try 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,29 +4,49 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+/**
+ * read_textfile - function that reads a text file and prints
+ *			it to the POSIX standard output.
+ * @filename: name of the file to process.
+ * @letters: number of chars to read
+ * Return: number of chars to print.
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t n;
-	char buf[13];
+	ssize_t w;
+	char *buf;
 
+	if (filename == NULL)
+		return (0);
+	buf  = malloc(letters);
+	if (buf == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
-
 	if (fd < 0)
 	{
-		printf("Error %d\n",fd);
-		return(0);
+		close(fd);
+		free(buf);
+		return (0);
 	}
 
-	n = read(fd, buf,14);
-
-	printf("Error %s\n",buf);
-
-	write(STDOUT_FILENO, buf, letters);
-
-	return(n);
-
+	n = read(fd, buf, letters);
+	if (n <  0)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
 	close(fd);
-
+	w = write(1, buf, n);
+	if (w < 0)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (w);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -3,4 +3,4 @@
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 
-#endif 
+#endif

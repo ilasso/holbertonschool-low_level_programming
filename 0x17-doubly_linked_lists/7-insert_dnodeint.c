02ad38d00ead44fc3f1b7664c992b1eca0c3be9d
ilasso <1204@holbertonschool.com>
2019-12-15 04:54:58 +0000

    7-insert_dnodeint.c last position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 /**
  * insert_dnodeint_at_index - function that inserts a new node
- *			      at a given position
  * @h:  head of a list
  * @idx:  position to add new node
  * @n:  new data node to add
@@ -9,11 +8,9 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new = NULL, *prev = NULL, *post = NULL, *temp = NULL;
+dlistint_t *new = NULL, *prev = NULL, *post = NULL, *temp = *h;
 unsigned int count = 0;
 
-if (h == NULL)
-	return (NULL);
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 	return (NULL);
@@ -21,29 +18,31 @@ new->n = n;
 if (*h == NULL)
 {	new->prev = NULL;
 	new->next = NULL;
-	*h = new;
-	count++; }
+	*h = new; }
 else
-{	temp = *h;
+{
 	if (idx != 0)
 	{
 		while (temp->next != NULL)
 		{
-			if (count == (idx - 1))
-				prev = temp;
-			else if (count == idx)
+			count == (idx - 1) ? (prev = temp) : (temp = temp);
+			if (count == idx)
 				break;
 			temp = temp->next;
 		count++; }
-		if (idx > count)
+		if (idx > (count + 1))
 			return (NULL);
+		if (idx == (count + 1))
+			{temp->next = new;
+			new->prev = temp;
+			return (new); }
 		post = temp;
-		prev->next = new;
 		post->prev = new;
+		prev->next = new;
 		new->prev = prev;
 		new->next = post; }
 	else
-	{	new->next = *h;
+		{new->next = *h;
 		new->prev = NULL;
 		(*h)->prev = new;
 		*h = new; } }
